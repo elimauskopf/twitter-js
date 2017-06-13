968e862fcf959ef1343d0b4999ce1d2aa440742b
Eli Mauskopf <elimauskopf7@gmail.com>
2017-06-13 14:55:39 -0400

    stuff

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,12 @@ router.get('/', function (req, res) {
     res.render('index', { tweets: tweets });
 });
 
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var list = tweetBank.find( {name: name} );
+  res.render( 'index', { list: list } );
+});
+
 router.use(express.static('public'));
 
 
