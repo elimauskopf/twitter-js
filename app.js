71ad642bce5b040ec77cbac3245d046a59ff2065
ev-dev <evan.reed1100@gmail.com>
2017-06-13 10:52:36 -0400

    configured nunjucks rendering

--- app.js
+++ app.js
@@ -5,18 +5,27 @@ const nunjucks = require('nunjucks');
 
 const app = express();
 
+app.set('vew engine', 'html');
+app.engine('html', nunjucks.render);
+
+nunjucks.configure('views', {
+    noCache: true
+});
+
 app.listen(3003, () => {
     console.log(chalk.yellow('Server listening on port 3003...'));
 });
 
-// app.use('/', (req, res, next) => {
-//     console.log(chalk.blue(req.method), req.url, chalk.green(res.statusCode.toString()));
-//     next();
-// });
+const locals = {
+    title: 'hi',
+    people: [
+        { name: 'Gandalf' },
+        { name: 'Frodo' },
+        { name: 'Hermione' }
+    ]
+};
 
-
-
-app.use(volleyball);
+nunjucks.configure('views');
 
 app.get('/', (req, res) => {
     res.send(chalk.blue('You got it!'));
@@ -26,19 +35,13 @@ app.get('/stuff', (req, res) => {
     res.send(chalk.red('Stuff here'));
 });
 
+
 app.get('/views', (req,res) => {
-    var locals = {
-    title: 'An Example',
-    people: [
-        { name: 'Gandalf'},
-        { name: 'Frodo' },
-        { name: 'Hermione'}
-    ]
-};
-    var env = nunjucks.configure('/views');
-    env.render('/views/index.html', locals, (err, output) =>{
-        console.log(output);
+    res.render('index.html', locals, (err, output) => {
+        if (err) throw err;
+        res.send(output);
     });
 
 });
 
+app.use(volleyball);
